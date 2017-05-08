1da08eab29f07321cb4cd969a22f66a19142fed2
Taylor Newill <tanewill@microsoft.com>
2017-05-08 15:46:58 -0400

    updated to support managed disks

--- install-Raw.sh
+++ install-Raw.sh
@@ -1,12 +1,11 @@
-yum update
+yum y update
 sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
 setenforce 0
 yum install -y kernel-headers --disableexcludes=all
 yum install -y kernel-devel --disableexcludes=all
 wget https://download.microsoft.com/download/7/6/B/76BE7A6E-E39F-436C-9353-F4B44EF966E9/lis-rpms-4.1.3-2.tar.gz
 tar xvzf lis-rpms-4.1.3-2.tar.gz
-cd LISISO
-./ uninstall.sh
+./uninstall.sh
 reboot
 ./install.sh
 reboot
