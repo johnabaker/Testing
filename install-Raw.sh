63374ea28f0dd46c7f0ff80d7873f9676f1f4a26
Taylor Newill <tanewill@microsoft.com>
2017-05-10 16:51:36 -0400

    raw

--- install-Raw.sh
+++ install-Raw.sh
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 yum y update
 sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
 setenforce 0
@@ -10,5 +11,7 @@ tar xvzf lis-rpms-4.1.3-2.tar.gz
 reboot
 ./install.sh
 reboot
+=======
+>>>>>>> parent of 3f6128d... added raw
 =======
 >>>>>>> parent of 3f6128d... added raw
