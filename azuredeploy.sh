b02879a36a310ab80a98bfe3a42a55259d883efa
Taylor Newill <tanewill@microsoft.com>
2017-05-25 18:53:52 -0400

    updated support package install

--- azuredeploy.sh
+++ azuredeploy.sh
@@ -44,7 +44,8 @@ is_master()
 install_pkgs()
 {
     yum -y install epel-release
-    yum -y install zlib zlib-devel bzip2 bzip2-devel bzip2-libs openssl openssl-devel openssl-libs gcc gcc-c++ nfs-utils rpcbind mdadm wget python-pip
+    #yum -y install zlib zlib-devel bzip2 bzip2-devel bzip2-libs openssl openssl-devel openssl-libs gcc gcc-c++ nfs-utils rpcbind mdadm wget python-pip
+    yum -y install nfs-utils sshpass nmap htop npm
 }
 
 # Creates and exports two shares on the master nodes:
