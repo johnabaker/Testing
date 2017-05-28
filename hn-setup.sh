5bf368ebc01ab3bd06939bde3782d4eb849fcd10
johnabaker <johnabaker@gmail.com>
2017-05-28 17:21:09 +0100

    Update hn-setup.sh

--- hn-setup.sh
+++ hn-setup.sh
@@ -3,7 +3,11 @@ USER=$1
 PASS=$2
 LICIP=$3
 
-if [ "$HOSTNAME" != "2h16mrcfd1" ]
+echo $USER > /tmp/deploy.log
+echo $PASS >> /tmp/deploy.log
+echo $LICIP >> /tmp/deploy.log
+
+if [ "$HOSTNAME" != "3h16mrcfd1" ]
 then
 echo "Im not the master" > /tmp/deploy.log
 exit
